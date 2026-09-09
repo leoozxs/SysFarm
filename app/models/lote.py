be45f670616675bfb9ec6e3b93773abdae905449
LEONARDO JAROSZEWSKI VALENÇA <05717919050@senacrs.edu.br>
2026-09-08 21:52:30 -0300

    Criando Model Saida, Lote e Fornecedor

--- app/models/lote.py
+++ app/models/lote.py
@@ -1,10 +1,13 @@
+from app.models.medicamento import Medicamento
+from app.models.fornecedor import Fornecedor
+
 class Lote:
-    def __init__(self, id, numero_lote, medicamento_id, fornecedor_id, validade):
-        self.id = id
-        self.numero_lote = numero_lote
-        self.medicamento_id = medicamento_id
-        self.fornecedor_id = fornecedor_id
-        self.validade = validade
+    def __init__(self, id, numero_lote, medicamento = Medicamento, fornecedor: Fornecedor, validade):
+        self._id = id
+        self._numero_lote = numero_lote
+        self._medicamento = medicamento
+        self._fornecedor = fornecedor
+        self._validade = validade
         
 @property
 def id(self):
@@ -24,19 +27,19 @@ def numero_lote(self, novo_numero_lote):
 
 @property
 def medicamento_id(self):
-    return self._medicamento_id
+    return self._medicamento
 
 @medicamento_id.setter
-def medicamento_id(self, novo_medicamento_id):
-    self._medicamento_id = novo_medicamento_id
+def medicamento_id(self, novo_medicamento):
+    self._medicamento = novo_medicamento
 
 @property
 def fornecedor_id(self):
-    return self._fornecedor_id
+    return self._fornecedor
 
 @fornecedor_id.setter
-def fornecedor_id(self, novo_fornecedor_id):
-    self._fornecedor_id = novo_fornecedor_id
+def fornecedor_id(self, novo_fornecedor):
+    self._fornecedor = novo_fornecedor
 
 @property
 def validade(self):
